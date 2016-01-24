bce2c27140ea8239b6767ed4ca7b96e619839a53
rsgc-goldsmith-j <jgoldsmith@rsgc.on.ca>
2016-01-24 12:02:21 -0500

    Implemented fix
    Calculated time using time_t variable, this method now handles the
    second wrap around correctly.

--- RGB_Matrix_V1.ino
+++ RGB_Matrix_V1.ino
@@ -43,6 +43,17 @@ static const int LED_BRIGHTNESS = 127;    // Brightness of LEDs
 boolean bitBoolean;
 int pSecond = 0;
 
+struct tm
+{
+  int y;    // Year
+  int mon;  // Month
+  int d;    // Day
+  int wd;   // Weekday
+  int h;    // Hour
+  int m;    // Minute
+  int s;    // Second
+} tm;
+
 Adafruit_NeoPixel matrix = Adafruit_NeoPixel(LED_NUM, DIN_PIN, NEO_GRB);
 
 void setup()
@@ -63,24 +74,19 @@ void loop()
 
   if (second() - pSecond == 1)    // Check if 1 second has passed
   {
-    bitTime(year() % CENTURY, YEAR, YEAR_ROW);
-    bitTime(month(), MONTH, MONTH_ROW);
-    bitTime(day(), DAY, DAY_ROW);
-    bitTime(weekday(), WEEKDAY, WEEKDAY_ROW);
-    bitTime(hour(), HOUR, HOUR_ROW);
-    bitTime(minute(), MINUTE, MINUTE_ROW);
-    bitTime(second() - 1, SECOND, SECOND_ROW);
-
-    pSecond = second();   // Set lagging value for seconds
-
-    if (pSecond == 59)
-      pSecond = 0;
-
-    Serial.println(second());
+    bitTime(tm.y % CENTURY, YEAR, YEAR_ROW);
+    bitTime(tm.mon, MONTH, MONTH_ROW);
+    bitTime(tm.d, DAY, DAY_ROW);
+    bitTime(tm.wd, WEEKDAY, WEEKDAY_ROW);
+    bitTime(tm.h, HOUR, HOUR_ROW);
+    bitTime(tm.m, MINUTE, MINUTE_ROW);
+    bitTime(tm.s + 1, SECOND, SECOND_ROW);
   }
+  Decode(now());
+  Serial.print(tm.s);
+  Serial.println();
+  delay(1000);
 }
-
-
 
 //
 // Using time as input set corresponding LEDs high.
@@ -99,4 +105,55 @@ void bitTime(int t, int tLength, int row)
 
     matrix.show();
   }
+}
+
+int Encode(int years, int months, int d, int hours, int minutes, int seconds)
+{
+  int dy;
+
+  static int days[] = // Days prior to start of month (origin-1)
+  {
+    0, -1, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333
+  };
+
+  dy = years - 1968; // Normalize year to leap year preceding epoch start
+  dy = (dy - 2) * 365 + (dy >> 2); // Multiply by 365.25, giving days in years
+  d -= !(years & 0x03) && months <= 2; // Reduce day if prior to leap point in leap year
+
+  return seconds + 60 * (minutes + 60 * (hours + 24 * (dy + days[months] + d)));
+}
+
+struct tm Decode(time_t ts)
+{
+  int d, m, y;
+//!  int ts;
+
+//!  static time_t     lastts;
+//!  static tm         tm;
+
+//!  if (tm == lastts) // Check if value to convert is same as last
+//!    return tm; // Return last result if so
+
+//!  ts = lastts = tm; // Time value to decode
+
+  tm.s = ts % 60; // Seconds
+  ts /= 60;
+
+  tm.m = ts % 60; // Minutes
+  ts /= 60;
+
+  tm.h = ts % 24; // Hours
+  ts /= 24;
+
+  ts = ts * 100 + 36525 * 2 - 5950; // Move start of epoch to 1 March 1968 and scale
+  y = (ts + 75) / 36525; // Years since start of epoch
+  d = (175 + ts - y * 36525) / 100 * 10; // Days since start of year (times 10)
+  m = (d - 6) / 306; // Months since start of year
+
+  tm.y = 1968 + y + (m >= 10); // Year
+  tm.mon = 1 + (m + 2) % 12; // Month
+  tm.d = (4 + d - m * 306) / 10; // Day
+  tm.wd = 1 + (5 + ts / 100) % 7; // Day of week (Sunday = 1)
+
+  return tm;
 }
